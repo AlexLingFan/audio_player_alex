afd55a17d2575ea4173c0e902a072d321c710d6b
alex.lingjiahua <alex.lingjiahua@fenda.com>
2022-01-25 19:04:50 +0800

    00

---
+++ asset_audio_player/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'audio_player_background_playlist.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: AudioPlayerBackgroundPlaylist(),
+  ));
+}
